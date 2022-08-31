5de983e2c956453855ae7d8356c0faa21ba35aa9
Agnieszka.Gabka <agn.gabka@gmail.com>
2022-08-31 16:54:09 +0200

    PlaceForm modified

--- RelaxHere/RelaxHereApp/forms.py
+++ RelaxHere/RelaxHereApp/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 
-from .models import Place
+from .models import Place, CLIMATE, ACTIVITY, CHILDREN_ACTIVITY, SCENERY, CONTINENT
 
 
-class PlaceForm(forms.ModelForm):
-
-    class Meta:
-        model = Place
-        fields = ("name", "visit_from", "visit_to", "climate", "activities", "children_friendly", "scenery", "continent",
-              "long_stay", "short_stay")
+class PlaceForm(forms.Form):
+    name = forms.CharField(required=False)
+    visit_from = forms.DateField(required=False)
+    visit_to = forms.DateField(required=False)
+    climate = forms.MultipleChoiceField(choices=CLIMATE, widget=forms.SelectMultiple(), required=False)
+    activities = forms.MultipleChoiceField(choices=ACTIVITY, widget=forms.SelectMultiple(), required=False)
+    children_friendly = forms.MultipleChoiceField(choices=CHILDREN_ACTIVITY, widget=forms.SelectMultiple(), required=False)
+    scenery = forms.MultipleChoiceField(choices=SCENERY, widget=forms.SelectMultiple(), required=False)
+    continent = forms.MultipleChoiceField(choices=CONTINENT, widget=forms.SelectMultiple(), required=False)
+    long_stay = forms.BooleanField(required=False)
+    short_stay = forms.BooleanField(required=False)
 
