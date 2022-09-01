e67d1a8f99332ea31fc4665b210f36301c446e2d
Agnieszka.Gabka <agn.gabka@gmail.com>
2022-09-01 19:18:56 +0200

    SearchResults modified, tables registered in admin.py

--- RelaxHere/RelaxHereApp/admin.py
+++ RelaxHere/RelaxHereApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Scenery, Activities, Climate, ChildrenActivities, Continent, Place
+
+
+admin.site.register(Scenery)
+admin.site.register(Activities)
+admin.site.register(Climate)
+admin.site.register(ChildrenActivities)
+admin.site.register(Continent)
+admin.site.register(Place)
--- RelaxHere/RelaxHereApp/views.py
+++ RelaxHere/RelaxHereApp/views.py
@@ -26,52 +26,52 @@ class SearchResultsView(View):
         form = PlaceForm(request.POST)
         places_list = []
         factors = []
-        if 'place' in request.POST:
+        if 'place' in form:
             place = request.POST['place']
             places = Place.objects.filter(name=place)
             if places:
                 places_list.append(places)
-        if 'visit_from' in request.POST:
+        if 'visit_from' in form:
             visit_from = request.POST['visit_from']
             places = Place.objects.filter(visit_from=visit_from)
             if places:
                 places_list.append(places)
-        if 'visit_to' in request.POST:
+        if 'visit_to' in form:
             visit_to = request.POST['visit_to']
             places = Place.objects.filter(visit_to=visit_to)
             if places:
                 places_list.append(places)
-        if 'climate' in request.POST:
+        if 'climate' in form:
             climate = request.POST['climate']
             places = Place.objects.filter(climate=climate)
             if places:
                 places_list.append(places)
-        if 'activities' in request.POST:
+        if 'activities' in form:
             activities = request.POST['activities']
             places = Place.objects.filter(activities=activities)
             if places:
                 places_list.append(places)
-        if 'children_friendly' in request.POST:
+        if 'children_friendly' in form:
             children_friendly = request.POST['children_friendly']
             places = Place.objects.filter(children_friendly=children_friendly)
             if places:
                 places_list.append(places)
-        if 'scenery' in request.POST:
+        if 'scenery' in form:
             scenery = request.POST['scenery']
             places = Place.objects.filter(scenery=scenery)
             if places:
                 places_list.append(places)
-        if 'continent' in request.POST:
+        if 'continent' in form:
             continent = request.POST['continent']
             places = Place.objects.filter(continent=continent)
             if places:
                 places_list.append(places)
-        if 'long_stay' in request.POST:
+        if 'long_stay' in form:
             long_stay = request.POST['long_stay']
             places = Place.objects.filter(long_stay=long_stay)
             if places:
                 places_list.append(places)
-        if 'short_stay' in request.POST:
+        if 'short_stay' in form:
             short_stay = request.POST['short_stay']
             places = Place.objects.filter(short_stay=short_stay)
             if places:
